604c8b02e33b8ae56dd01679391a9369b1338171
Girum Ibssa <ibssagirum@gmail.com>
2013-07-13 16:47:40 -0700

    More ios s gui stubs

---
+++ ios/Hang/Hang/CreateProposalViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateProposalViewController.h
+//  Hang
+//
+//  Created by Girum on 7/13/13.
+//  Copyright (c) 2013 Girum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateProposalViewController : UIViewController
+
+@end
---
+++ ios/Hang/Hang/CreateProposalViewController.m
@@ -0,0 +1,46 @@
+//
+//  CreateProposalViewController.m
+//  Hang
+//
+//  Created by Girum on 7/13/13.
+//  Copyright (c) 2013 Girum. All rights reserved.
+//
+
+#import "CreateProposalViewController.h"
+
+@interface CreateProposalViewController ()
+
+@end
+
+@implementation CreateProposalViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (IBAction)share:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)cancel:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
