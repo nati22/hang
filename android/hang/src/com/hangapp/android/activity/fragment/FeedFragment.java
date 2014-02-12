8c32e31dd6982dd00aa34186066999326080463c
Nati Tessema <nati.tessema@gmail.com>
2014-02-12 01:17:08 -0800

    Getting FeedFragment ready to hook up with xml for User fragment.

--- android/hang/src/com/hangapp/android/activity/fragment/FeedFragment.java
+++ android/hang/src/com/hangapp/android/activity/fragment/FeedFragment.java
@@ -7,17 +7,18 @@ import java.util.List;
 import org.joda.time.DateTime;
 import org.joda.time.Minutes;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,6 +48,9 @@ public final class FeedFragment extends SherlockFragment implements
 	private FriendsAdapter adapter;
 	private Button buttonRefresh;
 
+	// TODO: Turn this into an actual custom icon
+	private View myPicture;
+
 	// Member datum.
 	private ArrayList<User> incomingBroadcasts = new ArrayList<User>();
 
@@ -72,6 +76,7 @@ public final class FeedFragment extends SherlockFragment implements
 		}
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -84,6 +89,12 @@ public final class FeedFragment extends SherlockFragment implements
 		listViewFriends = (ListView) view
 				.findViewById(R.id.listViewFriendsFragment);
 		buttonRefresh = (Button) view.findViewById(R.id.buttonRefresh);
+
+		// TODO remove
+		View disView = (View) view.findViewById(R.id.homeUserFragment);
+		disView.setBackground(getResources().getDrawable(R.drawable.login_image));
+	//	myPicture = (View) disView.findViewById(R.id.userProfileIcon);
+	//	myPicture.setBackground(getResources().getDrawable(R.drawable.login_image));
 
 		// Set up the Adapter.
 		adapter = new FriendsAdapter(getActivity(), incomingBroadcasts);
@@ -148,129 +159,126 @@ public final class FeedFragment extends SherlockFragment implements
 		}
 
 		@Override
-      public View getView(int position, View convertView, ViewGroup parent) {
-              ViewHolder holder;
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
 
-              final User user = friends.get(position);
+			final User user = friends.get(position);
 
-              // Inflate the View if necessary.
-              if (convertView == null) {
-                      holder = new ViewHolder();
-                      convertView = inflater.inflate(R.layout.cell_friend_fragment,
-                                      null);
+			// Inflate the View if necessary.
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = inflater.inflate(R.layout.cell_friend_fragment, null);
 
-                      // Reference views
-                      holder.profilePictureView = (ProfilePictureView) convertView
-                                      .findViewById(R.id.profilePictureView);
-                      holder.textViewFriendName = (TextView) convertView
-                                      .findViewById(R.id.textViewFriendName);
-                      holder.textViewAvailabilityDescription = (TextView) convertView
-                                      .findViewById(R.id.textViewAvailabilityDescription);
-                      holder.statusIcon = (StatusIcon) convertView
-                                      .findViewById(R.id.imageButtonAvailability);
+				// Reference views
+				holder.profilePictureView = (ProfilePictureView) convertView
+						.findViewById(R.id.profilePictureView);
+				holder.textViewFriendName = (TextView) convertView
+						.findViewById(R.id.textViewFriendName);
+				holder.textViewAvailabilityDescription = (TextView) convertView
+						.findViewById(R.id.textViewAvailabilityDescription);
+				holder.statusIcon = (StatusIcon) convertView
+						.findViewById(R.id.imageButtonAvailability);
 
-                      Typeface champagneLimousinesBold = Typeface.createFromAsset(
-                                      getActivity().getApplicationContext().getAssets(),
-                                      Fonts.CHAMPAGNE_LIMOUSINES_BOLD);
-                      Typeface champagneLimousines = Typeface.createFromAsset(
-                                      getActivity().getApplicationContext().getAssets(),
-                                      Fonts.CHAMPAGNE_LIMOUSINES);
+				Typeface champagneLimousinesBold = Typeface.createFromAsset(
+						getActivity().getApplicationContext().getAssets(),
+						Fonts.CHAMPAGNE_LIMOUSINES_BOLD);
+				Typeface champagneLimousines = Typeface.createFromAsset(
+						getActivity().getApplicationContext().getAssets(),
+						Fonts.CHAMPAGNE_LIMOUSINES);
 
-                      holder.textViewFriendName.setTypeface(champagneLimousinesBold);
-                      holder.textViewAvailabilityDescription
-                                      .setTypeface(champagneLimousines);
+				holder.textViewFriendName.setTypeface(champagneLimousinesBold);
+				holder.textViewAvailabilityDescription
+						.setTypeface(champagneLimousines);
 
-                      convertView.setTag(holder);
-            //          Log.i("holder", "cell for " + user.getFirstName() + " == null");
-              } else {
-                      holder = (ViewHolder) convertView.getTag();
-           //           Log.i("holder", "cell for " + user.getFirstName() + " != null");
-              }
+				convertView.setTag(holder);
+				// Log.i("holder", "cell for " + user.getFirstName() + " == null");
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+				// Log.i("holder", "cell for " + user.getFirstName() + " != null");
+			}
 
-              holder.profilePictureView.setProfileId(user.getJid());
-              holder.textViewFriendName.setText(user.getFullName());
+			holder.profilePictureView.setProfileId(user.getJid());
+			holder.textViewFriendName.setText(user.getFullName());
 
-              // Reset the description
-              holder.textViewAvailabilityDescription.setText("");
+			// Reset the description
+			holder.textViewAvailabilityDescription.setText("");
 
-              // TODO: These sanity checks are already done in the StatusIcon...we
-              // should
-              // find a way to consolidate the code (set the desc from the
-              // StatusIcon???)
-              if (user.getAvailability() != null
-                              && user.getAvailability().isActive()) {
-                      // Set description if it's there
-                      if (user.getAvailability().getDescription() != null
-                                      && !user.getAvailability().getDescription()
-                                                      .equals("null")) {
-                              holder.textViewAvailabilityDescription.setText(user
-                                              .getAvailability().getDescription());
-                      }
-              }
+			// TODO: These sanity checks are already done in the StatusIcon...we
+			// should
+			// find a way to consolidate the code (set the desc from the
+			// StatusIcon???)
+			if (user.getAvailability() != null
+					&& user.getAvailability().isActive()) {
+				// Set description if it's there
+				if (user.getAvailability().getDescription() != null
+						&& !user.getAvailability().getDescription().equals("null")) {
+					holder.textViewAvailabilityDescription.setText(user
+							.getAvailability().getDescription());
+				}
+			}
 
-              holder.statusIcon.initialize(context, user, convertView);
-              holder.statusIcon.setAvailabilityColor(user.getAvailability());
-              holder.statusIcon.setOnClickListener(new OnClickListener() {
+			holder.statusIcon.initialize(context, user, convertView);
+			holder.statusIcon.setAvailabilityColor(user.getAvailability());
+			holder.statusIcon.setOnClickListener(new OnClickListener() {
 
-                      @Override
-                      public void onClick(View v) {
-                              if (!user.getAvailability().isActive()) {
-                                      restClient.sendNudge(user.getJid());
-                                      Toast.makeText(context,
-                                                      "Sending a nudge to " + user.getFirstName(),
-                                                      Toast.LENGTH_SHORT).show();
-                                      ((StatusIcon) v).setPressed(true);
-                              } else {
-                                      // Determine hrs and min left
-                                      int min = Minutes.minutesBetween(new DateTime(),
-                                                      user.getAvailability().getExpirationDate())
-                                                      .getMinutes();
+				@Override
+				public void onClick(View v) {
+					if (!user.getAvailability().isActive()) {
+						restClient.sendNudge(user.getJid());
+						Toast.makeText(context,
+								"Sending a nudge to " + user.getFirstName(),
+								Toast.LENGTH_SHORT).show();
+						((StatusIcon) v).setPressed(true);
+					} else {
+						// Determine hrs and min left
+						int min = Minutes.minutesBetween(new DateTime(),
+								user.getAvailability().getExpirationDate())
+								.getMinutes();
 
-                                      int hrs = 0;
-                                      while (min >= 60) {
-                                              hrs++;
-                                              min = min - 60;
-                                      }
+						int hrs = 0;
+						while (min >= 60) {
+							hrs++;
+							min = min - 60;
+						}
 
-                                      // Display remaining time to user
-                                      Toast.makeText(
-                                                      context,
-                                                      hrs + " hr" + ((hrs > 1) ? "s " : " ") + min
-                                                                      + " min remaining", Toast.LENGTH_SHORT)
-                                                      .show();
-                              }
-                      }
-              });
+						// Display remaining time to user
+						Toast.makeText(
+								context,
+								hrs + " hr" + ((hrs > 1) ? "s " : " ") + min
+										+ " min remaining", Toast.LENGTH_SHORT).show();
+					}
+				}
+			});
 
-              final int pos = position;
-              convertView.setOnClickListener(new OnClickListener() {
+			final int pos = position;
+			convertView.setOnClickListener(new OnClickListener() {
 
-                      @Override
-                      public void onClick(View v) {
-                              if (user.getProposal() == null) {
-                                      Toast.makeText(context, "User has no proposal",
-                                                      Toast.LENGTH_SHORT).show();
-                                      return;
-                              }
-                              Context context = FeedFragment.this.getActivity();
-                              User user = incomingBroadcasts.get(pos);
+				@Override
+				public void onClick(View v) {
+					if (user.getProposal() == null) {
+						Toast.makeText(context, "User has no proposal",
+								Toast.LENGTH_SHORT).show();
+						return;
+					}
+					Context context = FeedFragment.this.getActivity();
+					User user = incomingBroadcasts.get(pos);
 
-                              Intent proposalLeechIntent = new Intent(context,
-                                              ProfileActivity.class);
-                              proposalLeechIntent.putExtra(Keys.HOST_JID, user.getJid());
-                              context.startActivity(proposalLeechIntent);
+					Intent proposalLeechIntent = new Intent(context,
+							ProfileActivity.class);
+					proposalLeechIntent.putExtra(Keys.HOST_JID, user.getJid());
+					context.startActivity(proposalLeechIntent);
 
-                      }
-              });
+				}
+			});
 
-              return convertView;
-      }
+			return convertView;
+		}
 
 		class ViewHolder {
 			ProfilePictureView profilePictureView;
-//			ImageView greenRing;
-//			ImageView redRing;
-//			ImageView greyRing;
+			// ImageView greenRing;
+			// ImageView redRing;
+			// ImageView greyRing;
 			TextView textViewFriendName;
 			TextView textViewAvailabilityDescription;
 			// ImageView imageViewAvailability;
